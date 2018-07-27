17c8c63d0470fdf90d43584c6c0812e2363e076f
ccdunn <cdunnemail@gmail.com>
2018-07-26 23:38:38 -0400

    Bug fixes

--- cond_prob.py
+++ cond_prob.py
@@ -13,12 +13,17 @@ def comb_count(info, possibilities):
     if not n_hand:
         return (0, [])
     if n_hand == 1:
-        return possibilities[np.newaxis, ...]
+        return info[-1]*possibilities[np.newaxis, ...]
     n_poss = info.shape[1]
 
     combs = np.zeros((n_hand, n_poss) )
 
     this_card_poss = info[-1]*possibilities
+    # this_card_neg_poss = (1 - info[-1])*possibilities
+
+    # if np.sum(this_card_neg_poss) < np.sum(this_card_poss):
+    #     negate = True
+    #     this_card_poss = this_card_neg_poss
 
     for pp in np.where(this_card_poss)[0]:
         temp_poss = np.copy(possibilities)
@@ -27,13 +32,23 @@ def comb_count(info, possibilities):
         combs[:-1] += possibilities[pp]*sub_combs
         combs[-1, pp] += possibilities[pp]*np.sum(sub_combs[-1])
 
+
     return combs
 
 
 # info = np.array([[1, 1, 1, 1], [1, 1, 0, 0], [0, 1, 1, 1], [0, 0, 0, 1]])
 # possibilities = np.array([1, 1, 1, 1])
 
-info = np.ones((5, 5, 5))
+# info = np.ones((5, 5, 5))
+# possibilities = np.tile(np.array([3, 2, 2, 2, 1])[:, np.newaxis], (1, 5))
+
+info = np.ones((4, 5, 5))
 possibilities = np.tile(np.array([3, 2, 2, 2, 1])[:, np.newaxis], (1, 5))
 
-print(cond_prob(info, possibilities))
+# info = np.ones((5, 5, 5))
+# info[:, 0, :] = 0
+# possibilities = np.tile(np.array([3, 2, 2, 2, 1])[:, np.newaxis], (1, 5))
+
+probs = cond_prob(info, possibilities)
+
+print(probs)
